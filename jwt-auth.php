9f8cd7c35819658b9caf6b4536e51347bda9509a
Mulli Bahr <mulli@site2goal.co.il>
2020-08-15 18:45:47 +0300

    Initial version 1.2.7
    Fixed original 1.2.6 & Continue on my own.
    Added support for WP 5.5 (permission_callback in register rest endpoint)
    And few more older fixes, cany remember
    Tested & Works!

---
+++ jwt-auth.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * The plugin bootstrap file.
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://enriquechavez.co
+ * @since             1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       JWT Authentication for WP-API, DigmaTec version
+ * Plugin URI:        https://site2goal.co.il
+ * Description:       Based on Enrique Chavez. Extends the WP REST API using JSON Web Tokens Authentication as an authentication method. Updated for WP 5.5+
+ * Version:           1.2.7 - Mulli's
+ * Author:            Mulli Bahr fixes (Orig: Enrique Chavez, https://enriquechavez.co)
+ * Author URI:        https://site2goal.co.il
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       jwt-auth
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path(__FILE__) . 'includes/class-jwt-auth.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_jwt_auth()
+{
+    $plugin = new Jwt_Auth();
+// error_log("run_jwt_auth");
+    $plugin->run();
+}
+run_jwt_auth();
